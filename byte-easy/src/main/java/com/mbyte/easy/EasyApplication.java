814355a18ac212d89178c1a9b0705cc08b6ec824
“wth <wth@example.com>
2019-05-24 16:30:22 +0800

    123

---
+++ byte-easy/src/main/java/com/mbyte/easy/EasyApplication.java
@@ -0,0 +1,24 @@
+package com.mbyte.easy;
+
+import javafx.application.Application;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * SpringBoot启动类
+ */
+@SpringBootApplication
+public class EasyApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+}
---
+++ byte-easy/src/main/java/com/mbyte/easy/admin/entity/BdOldrecords.java
@@ -0,0 +1,40 @@
+package com.mbyte.easy.admin.entity;
+
+import com.mbyte.easy.common.entity.BaseEntity;
+import java.time.LocalDateTime;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author 吴天豪
+ * @since 2019-05-24
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Accessors(chain = true)
+public class BdOldrecords extends BaseEntity {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 创建时间
+     */
+    private LocalDateTime createtime;
+
+    /**
+     * 关键词
+     */
+    private String keyword;
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+
+}
